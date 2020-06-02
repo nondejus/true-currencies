f485cdbfe205bc4d42365146cb8e3bbced297957
Ivan Rukhavets <ivanruch@gmail.com>
2020-06-02 23:27:29 +0300

    liq addr

--- scripts/liquidator_reset.ts
+++ scripts/liquidator_reset.ts
@@ -5,8 +5,8 @@ import { LiquidatorResetFactory } from '../build/types/LiquidatorResetFactory'
 const resetAssuredFO = async () => {
   const provider = new ethers.providers.InfuraProvider('mainnet', '81447a33c1cd4eb09efb1e8c388fb28e')
   const wallet = new ethers.Wallet(process.argv[2], provider)
-  const proxy = OwnedUpgradeabilityProxyFactory.connect('0xa73343a7E918CEECcD2EaA1E4a350089c3b30F89', wallet)
-  const reset = LiquidatorResetFactory.connect('0xa73343a7E918CEECcD2EaA1E4a350089c3b30F89', wallet)
+  const proxy = OwnedUpgradeabilityProxyFactory.connect('0x1dfB7700E67e6046898c2FEfe160FB53fEC3A27c', wallet)
+  const reset = LiquidatorResetFactory.connect('0x1dfB7700E67e6046898c2FEfe160FB53fEC3A27c', wallet)
   const newImplementation = await (await new LiquidatorResetFactory(wallet).deploy({ gasPrice: 21_000_000_000 })).deployed()
   const oldImplementationAddress = await proxy.implementation()
   await (await proxy.upgradeTo(newImplementation.address, { gasPrice: 21_000_000_000 })).wait()
